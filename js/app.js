a14db664e7b229ec53d9c4170c941489c9f59ec1
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-18 17:26:41 +0100

    all event listeners working, moving onto the get request soon

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@ class PageHandler {
 
     this.userOutput = document.getElementById("userContent");
 		this.subredditOutput = document.getElementById("subredditContent");
+
+    this.homepage = document.getElementById("page1");
+    this.userpage = document.getElementById("page2");
+    this.subpage= document.getElementById("page3");
   }
 
   addEventListeners() {
@@ -21,13 +25,63 @@ class PageHandler {
         this.changePage(link.value);
     	}.bind(this));
 	  }.bind(this));
+
+    //user text input listener
+    this.userSubmit.onclick = function(event) {
+      event.preventDefault();
+      console.log("toot toot");
+    }.bind(this);
+
+    //subreddit text input
+    this.subredditSubmit.onclick = function(event) {
+      event.preventDefault();
+      console.log("pop pop");
+    }
   }
 
   changePage(targetPage) {
     if (targetPage != this.currentPage) {
       //do the animations here
-      
+      //hide the current page we're on
+      switch (this.currentPage) {
+        case 1:
+          this.homepage.style.display = 'none';
+          break;
+        case 2:
+          this.userpage.style.display = 'none';
+          break;
+        case 3:
+          this.subpage.style.display = 'none';
+          break;
+        default:
+          console.log("unknown page to hide");
+          break;
+      }
+
+      //change the page to target page
+      this.currentPage = targetPage;
+
+      //now we show the target page
+      switch (this.currentPage) {
+        case 1:
+          this.homepage.style.display = 'inline-block';
+          break;
+        case 2:
+          this.userpage.style.display = 'inline-block';
+          break;
+        case 3:
+          this.subpage.style.display = 'inline-block';
+          break;
+        default:
+          console.log("unknown page to show");
+          break;
+      }
     }
+  }
+
+  sendGetRequest() {
+
+    //return something
   }
 
 }
